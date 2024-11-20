90c6d785548ac6e908cb4593f508d7308cee3297
oscarxjauregui <19031369@itcelaya.edu.mx>
2024-11-19 23:49:02 -0600

    k

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 export default function Page() {
   return (
     <main className="contPrincipal">
-      <h1>Configuracion del perfil</h1>
+      <h1>Configuracion del perfil </h1>
     </main>
   );
 }
